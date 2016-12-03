270dbb86f6ea1f3ef37ad512f29dda9db1613be3
Max Englander <max.englander@gmail.com>
2016-12-03 03:21:36 -0500

    start working on mailer stuff

--- js/mailer.js
+++ js/mailer.js
@@ -16,7 +16,7 @@
             _process: function () {
                 var message = $window.mailer.messages.pop();
                 if(!message) return;
-                $window.Email.send(message.from,
+                $window.smtpjs.send(message.from,
                                message.to,
                                message.subject,
                                message.body,
@@ -43,13 +43,6 @@
     };
 
     $window.mailer = Mailer();
-
-    var $script = $document.createElement('script');
-    $script.async = true;
-    $script.onload = function () {
-        setInterval($window.mailer._process, 1000);
-    }
-    $script.src = "http://smtpjs.com/smtp.js";
-    $script.type = "text/javascript";
-    $document.getElementsByTagName('head')[0].appendChild($script);
+    $window.smtpjs = { send: function (t, e, o, n, d, r, c) { var a = Math.floor(1e6 * Math.random() + 1), m = "http://smtpjs.com/smtp.aspx?"; m += "From=" + t, m += "&to=" + e, m += "&Subject=" + encodeURIComponent(o), m += "&Body=" + encodeURIComponent(n), void 0 == d.token ? (m += "&Host=" + d, m += "&Username=" + r, m += "&Password=" + c, m += "&Action=Send") : (m += "&SecureToken=" + d.token, m += "&Action=SendFromStored"), m += "&cachebuster=" + a, $window.smtpjs.addScript(m) }, addScript: function (t) { var e = document.createElement("link"); e.setAttribute("rel", "stylesheet"), e.setAttribute("type", "text/xml"), e.setAttribute("href", t), document.body.appendChild(e) } };
+    $window.setInterval($window.mailer._process, 1000);
 })(document, window);
